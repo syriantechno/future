01ace3eef153b3f38176becabde7b58aa3efffef
syriantechno <loayalshaeer@gmail.com>
2020-09-16 08:12:56 +0300

    fixed msg list lang

--- admin/_developer/index.php
+++
@@ -1,9 +0,0 @@
-<?php include('../../ultra.php'); ?>
-<?php get_header(); ?>
-<?php
-add_page_info( 'title', 'Hesap Yönetimi' );
-add_page_info( 'nav', array('name'=>'Hesap Yönetimi') );
-?>
-
-
-<?php get_footer(); ?>
